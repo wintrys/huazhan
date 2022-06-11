9bf1f603aecc79602ed41fd8cbfee494cbe162e3
wintrys <forester_lee@163.com>
2022-06-12 01:48:47 +0800

    add weather

--- untils/weather.py
+++ untils/weather.py
@@ -1,7 +1,8 @@
 import requests
 # http://open.iciba.com/dsapi/ 每日一句
 def everyday():
-    url='http://open.iciba.com/dsapi/?date=2022-01-11'
+    url='http://open.iciba.com/dsapi/'
+    url_date = 'http://open.iciba.com/dsapi/?date=2022-01-11'
     url2='http://wthrcdn.etouch.cn/weather_mini?city=%E8%A5%BF%E5%AE%89'
     context=requests.get(url)
     # print(context.json())
